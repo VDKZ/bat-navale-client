0685e22ea34b290d3641ece674095d5a33c567d0
alexandre <alexcoudreuse@gmail.com>
2017-09-12 14:18:52 +0200

    modif

--- bataille.js
+++ bataille.js
@@ -1,48 +1,21 @@
-
-// $('#adversaire').on('click','td', function(){
-//
-//   var $this = $(this);
-//   var col   = $this.index();
-//   var row   = $this.closest('tr').index();
-//
-//   var webMethod = "http://MyWebService/Web.asmx/GetInfoByDates";
-//   var parameters = "{'X':'" + col + "','Y':'" + row + "'}";
-//
-//   alert( parameters );
-//
-//   $(this).addClass('rate');
-//
-//   $.ajax({
-//       type: "POST",
-//       url: webMethod,
-//       data: parameters,
-//       contentType: "application/json; charset=utf-8",
-//       dataType: "json",
-//       success: function(data) {
-//           if(data.success){
-//             $(this).addClass('touche');
-//           } else {
-//             $(this).addClass('rate');
-//           }
-//       }
-//   });
-// })
-
 var socket = io.connect('localhost:3000');
 
 $('#adversaire').on('click','td', function(event){
   event.preventDefault();
 
   var $this = $(this);
-  var col   = $this.index();
-  var row   = $this.closest('tr').index();
-  var parameters = "{'X':'" + col + "','Y':'" + row + "'}";
+  var col   = parseInt(($this.index())-1);
+  var row   = parseInt(($this.closest('tr').index())-1);
+  var test = {
+    x: col,
+    y: row
+  };
 
-  socket.emit('tir', { tir: parameters });
+  socket.emit('tir', { tir: test });
 });
 
-socket.on('callback', function(data) {
-  console.log(data);
-  alert(data);
-  // Print the data.data somewhere...
+socket.on('tir', function(data) {
+  console.log(data.hit);
+  //  $(this).addClass('rate');
+  //  $(this).addClass('touche');
 });
